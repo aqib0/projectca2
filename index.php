5bf125275f36a281aba111906057c7aa04a86b5d
aqib0 <38186181+aqib0@users.noreply.github.com>
2020-04-10 15:34:41 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php include('server.php'); 
+
+if(empty($_SESSION['username'])) {
+	header('location: login.php');
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+     <title> Registration system </title>
+	 <link rel="stylesheet" type="text/css" href="style.css">
+	 <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+</head>
+<body>	 
+   <div class="container">
+  <div class="login-box"></div>
+   <div class="row">
+   <div class="col-md-10 login-left" >
+       <h2>Home Page</h2>
+	<div class="header">
+        <h2>WECOME USER</h2>
+</div>		
+	<div class="content">
+	   <?php if (isset($SESSION['success'])):?>
+	      <div class="error success">
+		    <h3>
+			   <?php 
+			       echo $_SESSION['success'];
+				   unset($_SESSION['success']);
+				 ?>
+			</h3>
+			</div>
+	   <?php endif ?>
+	   
+	   <?php if (isset($_SESSION['username'])): ?>
+	   <p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+	   <p><a href="index.php?logout='1'" style="color: red;">Logout</a></p>
+	 <?php endif ?>
+   </div>	 
+	</div>
+</div>
+</div>	
+	   
+</body>
+</html>
